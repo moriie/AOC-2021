654731f74c300fdd0541a5d3c7f4cbf2b902ba21
jwy5140 <jwy5140@gmail.com>
2021-12-22 01:11:40 -0500

    Day 21 part 1 solution.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,57 @@ function App() {
   let arr = [];
   let n;
   let j;
-  let len;
   let testArr;
   let ans = 0;
+  let pos1 = 8;
+  let pos2 = 9;
+  let score1 = 0;
+  let score2 = 0;
+  let turn = true
+  let dice = 1;
+  let rolls = 0;
+
+  const rollDice = (pos) => {
+    let r1 = dice;
+    checkDice()
+    let r2 = dice;
+    checkDice()
+    let r3 = dice;
+    checkDice()
+
+    pos = (pos+r1+r2+r3)%10
+    return pos === 0 ? 10 : pos
+
+  }
+  const checkDice = () => {
+    if (dice === 100){
+      dice = 1;
+    }
+    else {
+      dice++
+    }
+  }
 
   const Answer = () => {
-    fetch("./values.txt")
-    .then((res) => res.text())
-    .then((txt) => {
-      testArr = txt.split(",")
+
+    while (score1 < 1000 && score2 < 1000){
+      if (turn){
+        pos1 = rollDice(pos1);
+        score1 = score1+pos1;
+      }
+      else {
+        pos2 = rollDice(pos2);
+        score2 = score2+pos2;
+      }
+      turn = !turn
+      rolls=rolls+3
+    }
+    console.log(score1, score2, rolls)
+    // fetch("./values.txt")
+    // .then((res) => res.text())
+    // .then((txt) => {
+      // testArr = txt.split(",")
+
       
       // testArr.forEach((str, index)=>{
   //       arr[index] = str.split('')
@@ -53,7 +95,7 @@ function App() {
   //         Flash(j,k)
   //       }
   //     }
-    })
+    // })
   }
 
   Answer();
